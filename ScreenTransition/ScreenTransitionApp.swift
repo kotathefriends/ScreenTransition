556f00aa9af99bbd8a267f2f1636603a502890bb
kotathefriends <5070kouta@gmail.com>
2023-10-14 21:06:49 +0900

    Initial Commit

---
+++ ScreenTransition/ScreenTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTransitionApp.swift
+//  ScreenTransition
+//
+//  Created by KOTA TAKAHASHI on 2023/10/14.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
